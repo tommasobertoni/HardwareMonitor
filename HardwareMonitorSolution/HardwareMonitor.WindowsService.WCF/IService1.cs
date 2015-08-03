22778981da1e004bfb373657af550b3a62533db0
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-08-03 14:10:50 +0200

    WCF Service project
    configuration for tcp binding
    contract for cpu temperature api

---
+++ HardwareMonitorSolution/HardwareMonitor.WindowsService.WCF/IService1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HardwareMonitor.WindowsService.WCF
+{
+    [ServiceContract]
+    public interface IHardwareMonitorWCFContract
+    {
+        [OperationContract]
+        float? GetAvgCPUsTemperature();
+
+        [OperationContract]
+        float? GetCPUTemperature(int cpuIndex);
+    }
+}
