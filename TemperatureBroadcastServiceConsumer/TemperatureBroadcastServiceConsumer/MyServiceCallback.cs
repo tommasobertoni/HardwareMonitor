66c0b561f32cafc2e5beda7d69b03420cbabab68
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-08-16 15:59:44 +0200

    (test) console application project which consumes the TemperatureWCFClientBroadcastClient service

---
+++ TemperatureBroadcastServiceConsumer/TemperatureBroadcastServiceConsumer/MyServiceCallback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TemperatureBroadcastServiceConsumer.TemperatureBroadcastServiceReference;
+
+namespace TemperatureBroadcastServiceConsumer
+{
+    public class MyServiceCallback : TemperatureBroadcastServiceReference.ITemperatureWCFClientBroadcastCallback
+    {
+        void ITemperatureWCFClientBroadcastCallback.AvgCPUsTemperatureChanged(float temperature)
+        {
+            Console.WriteLine($"Received: {temperature}");
+        }
+    }
+}
---
+++ TemperatureBroadcastServiceConsumer/TemperatureBroadcastServiceConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace TemperatureBroadcastServiceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var callback = new MyServiceCallback();
+            
+            var instanceContext = new InstanceContext(callback);
+            var service = new TemperatureBroadcastServiceReference.TemperatureWCFClientBroadcastClient(instanceContext);
+            var client = service.ChannelFactory.CreateChannel();
+            client.SubscribeClient();
+            Console.WriteLine("Client subscribed");
+
+            Console.ReadLine();
+            client.UnsubscribeClient();
+            Console.WriteLine("Client unsubscribed");
+            service.Close();
+            Console.WriteLine("Service closed");
+            Console.ReadLine();
+        }
+    }
+}
