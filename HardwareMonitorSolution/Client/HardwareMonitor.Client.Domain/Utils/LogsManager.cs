118bd56bce688023432798623c1415c635442eda
Tommaso Bertoni <bertonitommaso@gmail.com>
2015-09-14 23:19:00 +0200

    LogLevel Flags
    LogObserver

--- HardwareMonitorSolution/Client/HardwareMonitor.Client.Domain/Utils/LogsManager.cs
+++ HardwareMonitorSolution/Client/HardwareMonitor.Client.Domain/Utils/LogsManager.cs
@@ -1,31 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace HardwareMonitor.Client.Domain.Utils
 {
     public static class LogsManager
     {
-        private const string _SEPARATOR = "--------------------------------";
-        private static readonly string _LOGS_FOLDER = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\HardwareMonitor Logs\\";
-
+        [Flags]
         public enum LogLevel
         {
-            VERBOSE, DEBUG, WARNING, ERROR
+            None = 0,
+            Verbose = 1 << 0,
+            Debug =   1 << 1,
+            Warning = 1 << 2,
+            Error =   1 << 3,
+            Temp =    1 << 4
         };
 
-        static LogsManager()
+        public abstract class LogObserver
         {
-            if (!Directory.Exists(_LOGS_FOLDER))
-                Directory.CreateDirectory(_LOGS_FOLDER);
+            public LogLevel LogLevelsObserved { get; private set; } = LogLevel.None;
+
+            public abstract void OnLog(string message, LogLevel level);
+
+            protected void Subscribe() => _observers.AddLast(this);
+
+            protected void Unsubscribe() => _observers.Remove(this);
         }
 
-        public static void Log(string message, LogLevel level = LogLevel.DEBUG)
+        private const string _SEPARATOR = "--------------------------------";
+        private static readonly string _LOGS_FOLDER = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\HardwareMonitor Logs\\";
+        private static readonly string _TEMP_LOGS_FOLDER = $"{_LOGS_FOLDER}\\_TEMP_LOGS_FOLDER\\";
+
+        private static LinkedList<LogObserver> _observers = new LinkedList<LogObserver>();
+
+        public static void Log(object message, LogLevel level = LogLevel.Debug)
         {
-            File.AppendAllText(
-                LogFilePath(level),
-                $"{DateTime.UtcNow}\n{message}\n{_SEPARATOR}\n");
+            Log(new object[] { message }, level);
         }
 
-        public static string LogFilePath(LogLevel level = LogLevel.DEBUG) => $"{_LOGS_FOLDER}{level.ToString().ToLower()}.log";
+        public static void Log(object[] messages, LogLevel level = LogLevel.Debug)
+        {
+            if (!Directory.Exists(_LOGS_FOLDER)) Directory.CreateDirectory(_LOGS_FOLDER);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var message in messages)
+                sb.AppendLine(message.ToString());
+
+            var buildedMessage = sb.ToString();
+
+            foreach (var path in LogFilesPath(level))
+                File.AppendAllText(path, $"{DateTime.UtcNow}\n{buildedMessage}\n{_SEPARATOR}\n");
+
+            try
+            {
+                lock (_observers)
+                {
+                    foreach (var observer in _observers)
+                    {
+                        lock (observer)
+                        {
+                            if (observer.LogLevelsObserved.HasFlag(level)) observer.OnLog(buildedMessage, level);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText(LogFilesPath(LogLevel.Error)[0], $"Log to observers (level {level}): {ex}");
+            }
+        }
+
+        public static string GetTempLogsFolderPath()
+        {
+            if (!Directory.Exists(_TEMP_LOGS_FOLDER)) Directory.CreateDirectory(_TEMP_LOGS_FOLDER);
+            return _TEMP_LOGS_FOLDER;
+        }
+
+        public static string[] LogFilesPath(LogLevel lv = LogLevel.Debug)
+        {
+            var levels = lv.ToString().ToLower().Split(',');
+            var paths = new string[levels.Length];
+
+            for (int i = 0; i < levels.Length; i++)
+                paths[i] = $"{_LOGS_FOLDER}{levels[i].Trim()}.log";
+
+            return paths;
+        }
     }
 }
